8e9adf8eb7a3ba38ec078eb9da2e3e7d48f5ae65
Vicky Daniel <vicky16898@gmail.com>
2024-09-19 19:21:48 -0400

    Reorder Landing Page

--- src/TOC.tsx
+++ src/TOC.tsx
@@ -5,8 +5,8 @@ export default function TOC() {
         <ul>
             <li><Link to="/Labs">Labs</Link></li>
             <li><Link to="/Kanbas">Kanbas</Link></li>
-            <li><a href="https://github.com/vicky16898/kanbas-react-web-app">Github Source Code</a></li>
             <li><Link to="/">Landing Page</Link></li>
+            <li><a href="https://github.com/vicky16898/kanbas-react-web-app">Github Source Code</a></li>
         </ul>
     );
 }

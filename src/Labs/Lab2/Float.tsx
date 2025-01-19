ca452c40bb71f227981743441521d505b7a4f0c4
fchristofrank <fchristofrank@gmail.com>
2025-01-19 18:14:37 -0500

    CSS A2

---
+++ src/Labs/Lab2/Float.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const Float: React.FC = () => {
+    return (
+        <div id="wd-float-divs">
+            <h2>Float</h2>
+            <div>
+                <img className="wd-float-right"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, necessitatibus. In nisi aperiam fugiat repellendus excepturi sint quidem quas sit blanditiis atque eligendi deleniti, minus ipsa, iste repellat repudiandae eos incidunt neque commodi nesciunt? Dolor, repudiandae odit temporibus consequuntur libero esse excepturi laudantium voluptatibus dolore praesentium quidem est mollitia officiis laborum provident voluptates veritatis sapiente qui ea! Eaque quod ullam autem placeat quam animi, laborum perferendis inventore nihil ad sequi dolorum voluptate nisi voluptatem beatae illo vel blanditiis ipsam nostrum officiis maiores. Aspernatur similique quidem modi itaque unde fuga ipsa, eaque nihil cum. Asperiores voluptas vitae neque iure eligendi eaque.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi voluptas accusamus quos eveniet. A ipsa numquam vero consequuntur, ab quasi itaque voluptatem pariatur beatae, cum temporibus, consectetur porro alias neque provident impedit sequi adipisci? Unde dolore doloremque architecto! Perspiciatis ratione cupiditate, iste provident dolor nemo dignissimos reiciendis atque, dolorum ipsam minus veritatis velit eum dolores! Aliquam et corporis fugit cumque excepturi facilis, recusandae repellendus est reiciendis praesentium quam adipisci suscipit nostrum iure animi hic deserunt. Doloribus, quasi sequi tempore numquam quis voluptatibus magni eius id blanditiis rem? Tenetur molestiae iure nemo assumenda nihil atque quas inventore dolorem accusamus voluptates? Ab?
+                <img className="wd-float-left"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, voluptatibus nam perspiciatis dignissimos vitae animi consectetur soluta culpa, quasi voluptates eius doloribus, aut perferendis aperiam sequi praesentium voluptatum. Placeat sit ullam officiis hic rem voluptatum possimus repellendus incidunt recusandae veritatis temporibus aliquam animi porro laboriosam natus nostrum ipsa expedita, atque iusto quam sint? Exercitationem quod fugiat repellendus perspiciatis dolorum molestiae dicta natus deleniti ducimus laudantium voluptatem, accusamus modi aut ut culpa odit ipsum reprehenderit porro necessitatibus? Id cupiditate facilis ipsam nihil, vel sunt reiciendis qui quia quisquam consequuntur accusamus doloribus aperiam dolorem molestias iusto facere voluptatum! Ullam esse maiores distinctio.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ipsum eveniet inventore consequatur aut nesciunt dolor quas unde! Minima, sunt laborum. Ea modi quis quam soluta ad quisquam aspernatur natus nisi repellendus dolorum culpa animi omnis, eum eos, sunt pariatur assumenda rem totam minus eaque deserunt quae suscipit quia aliquam. Fugiat voluptates tenetur omnis quia libero eum quas impedit at rem voluptate repudiandae iure cupiditate ea, labore corporis reprehenderit, magni dicta, consequatur aliquid optio modi. Dolore sequi eveniet, laboriosam ipsa eligendi impedit dolorem dolorum perferendis tempora blanditiis explicabo illo aperiam libero? Facere ipsam dolores tenetur quo rem odio provident illo.
+                <img className="wd-float-right"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, placeat modi! Aut non, tempora eligendi ab dolore error nobis suscipit cupiditate pariatur? Tempore accusantium recusandae ut porro odio suscipit veritatis dolor libero corrupti nulla rerum soluta, molestiae adipisci deserunt quasi fugiat maiores ipsum dolore atque obcaecati nam omnis ducimus! Adipisci temporibus quasi veniam aspernatur in suscipit minus dolorum, magnam porro fuga, dicta vel dolorem quo harum, quisquam accusamus blanditiis quidem omnis nam id? Doloribus aperiam iure dolore voluptatum sed tempora obcaecati vero nulla praesentium in ea totam eaque deleniti fuga a quae laborum odio maiores qui, voluptas nam deserunt asperiores?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi fuga animi quo, voluptate eligendi officiis! Eos ab molestiae voluptatem, perspiciatis sit ex? Quo eaque saepe aspernatur ut, consequuntur eos eum dignissimos corrupti mollitia recusandae ducimus quae inventore exercitationem, temporibus, dolore totam rem. Quia, alias esse praesentium atque temporibus accusamus! Beatae, inventore? Velit reprehenderit explicabo nesciunt exercitationem necessitatibus. Veritatis, provident eum eos eius veniam maxime reiciendis dicta et earum optio repudiandae odio at! Vero atque fugit excepturi libero accusantium tempore voluptas quibusdam hic ipsa dolorum! Rem obcaecati labore quas perferendis velit aliquam asperiores repellendus facilis consequatur, quibusdam reiciendis tempora, saepe ipsum?
+                <img className="wd-float-left"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic perspiciatis deserunt harum obcaecati tenetur suscipit. Tenetur error debitis voluptatibus exercitationem corporis aliquid esse reprehenderit aperiam accusantium impedit? Maiores fugit illo, porro delectus voluptate numquam fugiat doloribus? Beatae, labore, explicabo repellat rerum ut autem sit repellendus, cupiditate adipisci magnam qui nulla consequuntur! Dignissimos, autem expedita natus magnam a repudiandae possimus impedit iste odio amet repellendus deserunt eaque! Doloremque modi autem accusamus omnis officia illo! Quam laudantium natus temporibus voluptatibus nesciunt, dolore similique optio quibusdam quisquam assumenda totam nam eaque corrupti quis! Ullam possimus, necessitatibus sint repellat veniam eaque quidem laboriosam ea.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos ipsum, obcaecati commodi, dolores, veritatis assumenda ut itaque sapiente minus vitae placeat deserunt fugiat. Enim architecto placeat quos recusandae. Ad magni perferendis doloribus quisquam, autem rem veritatis ex ipsam officia qui animi? Inventore, ad quod similique facere molestias aut itaque consequatur culpa repudiandae ullam id odio magni sequi cumque perspiciatis soluta ea consectetur dolorum iste. Consequuntur fuga nesciunt quasi obcaecati explicabo ipsum reprehenderit soluta adipisci qui tempore quisquam labore dolorum, quos unde deserunt in vitae ad! Doloribus aliquam perspiciatis deleniti praesentium ducimus esse officiis nesciunt. Voluptatibus fugit odio recusandae reiciendis quas!
+                <div className="wd-float-done"></div>
+            </div>
+            <div>
+                <div id="wd-float-divs">
+                    <h2>Float</h2>
+                    <div>
+                        <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+                            Yellow </div>
+                        <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+                            Blue </div>
+                        <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+                            Red </div>
+                        <img className="wd-float-right"
+                            src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+                        <div className="wd-float-done"></div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Float;

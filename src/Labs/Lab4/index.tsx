147babfbb17f1900e14d799cf406eda7fd768653
fchristofrank <fchristofrank@gmail.com>
2025-03-02 10:41:01 -0500

    js updates

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,6 +1,13 @@
 import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariables";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateCounter from "./ParentStateComponent";
 
 function sayHello() {
     alert("Hello");
@@ -12,6 +19,13 @@ export default function Lab4() {
             <ClickEvent />
             <PassingDataOnEvent />
             <PassingFunctions theFunction={sayHello} />
+            <EventObject />
+            <Counter/>
+            <BooleanStateVariables/>
+            <StringStateVariables/>
+            <DateStateVariable/>
+            <ArrayStateVariable/>
+            <ParentStateCounter/>
         </div>
     );
 }

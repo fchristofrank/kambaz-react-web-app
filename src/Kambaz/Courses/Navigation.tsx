a372d76e51f2b76d98dbc3150cde131f79b524d7
fchristofrank <fchristofrank@gmail.com>
2025-03-13 02:31:52 -0400

    chagne

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -5,7 +5,6 @@ export default function CoursesNavigation() {
   const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <div className="wd-sem-title mb-3 ms-3 me-3">202510_2 Fall 2024 Semester...</div>
       {links.map((link) => (
         <Link to={`/kambaz/Courses/${cid}/${link}`} id={`wd-course-${link}-link`}
           className={`list-group-item border-0 ${pathname.includes(link) ? "active border" : "text-danger"}`}>{link}</Link>

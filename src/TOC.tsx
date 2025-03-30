5c6206ec80c54d6df1d5fc7542701b224be967e6
fchristofrank <fchristofrank@gmail.com>
2025-03-29 20:04:36 -0400

    change

--- src/TOC.tsx
+++ src/TOC.tsx
@@ -7,7 +7,6 @@ export default function TOC() {
                 <ul>
                     <li><Link to="#/Labs">Labs</Link></li>
                     <li><Link to="#/Kambaz">Kambaz</Link></li>
-                    <li><a id="wd-github" href="https://github.com/vicky16898/kambaz-react-web-app">Github Source Code</a></li>
                 </ul>
             </div>
 

8c43949a654cd9c720d673bec7ec448e4788a5c0
fchristofrank <fchristofrank@gmail.com>
2025-04-14 10:20:56 -0400

    Build Errors

--- src/Kambaz/Courses/Comments/ModuleControlButtons.tsx
+++ src/Kambaz/Courses/Comments/ModuleControlButtons.tsx
@@ -1,9 +1,9 @@
-import { IoEllipsisVertical } from "react-icons/io5";
-import GreenCheckmark from "./GreenCheckmark";
 import { AiOutlinePlus } from "react-icons/ai";
 import { FaTrash } from "react-icons/fa";
 import { FaPencil } from "react-icons/fa6";
+import { IoEllipsisVertical } from "react-icons/io5";
 import { useSelector } from "react-redux";
+import GreenCheckmark from "./GreenCheckmark";
 export default function ModuleControlButtons({ moduleId, deleteModule, editModule }: { moduleId: string; deleteModule: (moduleId: string) => void; editModule: (moduleId: string) => void; } ) {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const isFaculty = currentUser.role === 'FACULTY' || currentUser.role === 'ADMIN';
